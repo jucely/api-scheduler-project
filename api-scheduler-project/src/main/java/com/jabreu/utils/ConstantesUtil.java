62dc2b7698b0975c61efecc2ffd3def87da2c2d1
jucely <jucely@jucely-Inspiron-7580>
2020-11-14 11:27:46 -0300

    Correção de final constant

--- api-scheduler-project/src/main/java/com/jabreu/utils/ConstantesUtil.java
+++ api-scheduler-project/src/main/java/com/jabreu/utils/ConstantesUtil.java
@@ -2,25 +2,25 @@ package com.jabreu.utils;
 
 public class ConstantesUtil {
 
-	public static Integer HOUR_START_MORNING_SESSION = 9;
-	public static Integer HOUR_START_AFTERNOON_SESSION = 13;
+	public static final Integer HOUR_START_MORNING_SESSION = 9;
+	public static final Integer HOUR_START_AFTERNOON_SESSION = 13;
 
-	public static Integer FIRST_DAY_HOUR = 0;
-	public static Integer LAST_DAY_HOUR = 23;
+	public static final Integer FIRST_DAY_HOUR = 0;
+	public static final Integer LAST_DAY_HOUR = 23;
 
-	public static Integer LENGTHS_MORNING_SESSION = 180;
-	public static Integer LENGTHS_AFTERNOON_SESSION = 240;
+	public static final Integer LENGTHS_MORNING_SESSION = 180;
+	public static final Integer LENGTHS_AFTERNOON_SESSION = 240;
 
-	public static String LUNCH_EVENT_NAME = "Lunch";
-	public static String LUNCH_TIME_START = "12:00PM";
+	public static final String LUNCH_EVENT_NAME = "Lunch";
+	public static final String LUNCH_TIME_START = "12:00PM";
 
-	public static String NETWORKING_EVENT_NAME = "Networking Event";
-	public static String NETWORKING_TIME_START = "05:00PM";
+	public static final String NETWORKING_EVENT_NAME = "Networking Event";
+	public static final String NETWORKING_TIME_START = "05:00PM";
 
-	public static String NUMBER_REGEX = "\\d+";
+	public static final String NUMBER_REGEX = "\\d+";
 
-	public static String LIGHTNING_EVENT_DURATION = "5";
+	public static final String LIGHTNING_EVENT_DURATION = "5";
 
-	public static String NORMAL_EVENT_REGEX = "(\\D+)(\\d+)(min|MIN|Min|Minuto|MINUTO|minuto)";
-	public static String LIGHTNING_EVENT_REGEX = "\\D+";
+	public static final String NORMAL_EVENT_REGEX = "(\\D+)(\\d+)(min|MIN|Min|Minuto|MINUTO|minuto)";
+	public static final String LIGHTNING_EVENT_REGEX = "\\D+";
 }
